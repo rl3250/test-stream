1e686c3c65b36af60fe7b50db09d1d3e54161f82
Ruize Li <99834841+rl3250@users.noreply.github.com>
2023-03-22 01:05:48 -0400

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -17,20 +17,23 @@ const handler = async (req: Request): Promise<Response> => {
     return new Response("No prompt in the request", { status: 400 });
   }
 
-  const payload: OpenAIStreamPayload = {
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 200,
-    stream: true,
-    n: 1,
-  };
+//   const payload: OpenAIStreamPayload = {
+//     model: "gpt-3.5-turbo",
+//     messages: [{ role: "user", content: prompt }],
+//     temperature: 0.7,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0,
+//     max_tokens: 200,
+//     stream: true,
+//     n: 1,
+//   };
 
-  const stream = await OpenAIStream(payload);
-  return new Response(stream);
+//   const stream = await OpenAIStream(payload);
+  const stream_res = await fetch('https://muddy-sunset-3486.fly.dev/stream')
+  
+  
+  return new Response(stream_res.body);
 };
 
 export default handler;
